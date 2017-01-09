7702bea42b7f5a248b47a1c819d50cd6efdc014d
Benjamin Große <ste3ls@gmail.com>
2017-01-09 10:15:52 +0100

    chatbox style

--- src/Game/Chat.elm
+++ src/Game/Chat.elm
@@ -10,39 +10,53 @@ import Html.Events exposing (..)
 import Material.Card as Card
 import Material.Options as Options exposing (cs, css)
 import Material.Textfield as Textfield
+import Material.Elevation
+import Material.Icon as Icon
+import Material.Button as Button
 
 
 chatBox : Model -> Html Types.Msg
 chatBox model =
-    Card.view [ cs "chatbox" ]
-        [ Card.text []
-            [ div [ class "chatbox--log" ]
-                (List.map
-                    (\c ->
-                        case c of
-                            LogChat user message ->
-                                div [ class "chatbox--line--chat" ]
-                                    [ Html.span []
-                                        [ Html.text <| user ++ ":" ]
-                                    , Html.span []
-                                        [ Html.text message ]
-                                    ]
+    Card.view
+        [ cs "chatbox"
+        , Material.Elevation.e2
+        ]
+        [ Card.media [ cs "chatbox--log" ]
+            (List.map
+                (\c ->
+                    case c of
+                        LogChat user message ->
+                            div [ class "chatbox--line--chat" ]
+                                [ Html.span []
+                                    [ Html.text <| user ++ ":" ]
+                                , Html.span []
+                                    [ Html.text message ]
+                                ]
 
-                            LogJoin user ->
-                                div [ class "chatbox--line--join" ]
-                                    [ Html.text <| user ++ " joined" ]
+                        LogJoin user ->
+                            div [ class "chatbox--line--join" ]
+                                [ Html.text <| user ++ " joined" ]
 
-                            LogLeave user ->
-                                div [ class "chatbox--line--leave" ]
-                                    [ Html.text <| user ++ " left" ]
-                    )
-                    model.backend.chatLog
+                        LogLeave user ->
+                            div [ class "chatbox--line--leave" ]
+                                [ Html.text <| user ++ " left" ]
                 )
-            ]
-        , Card.actions []
-            [ Html.form [ onSubmit (Types.GameMsg <| SendChat "hi") ]
+                model.backend.chatLog
+            )
+        , Card.actions [ cs "chatbox--actions" ]
+            [ Html.form [ onSubmit (Types.GameMsg <| SendChat "hi"), class "chatbox--actions-form" ]
                 [ input model
-                , button [ type' "submit", hidden True ] [ text "->" ]
+                , Button.render
+                    Types.Mdl
+                    [ 0 ]
+                    model.mdl
+                    [ Button.primary
+                    , Button.colored
+                    , Button.ripple
+                    , Button.type' "submit"
+                    , cs "chatbox--actions-button"
+                    ]
+                    [ Icon.i "keyboard_return" ]
                 ]
             ]
         ]
@@ -55,4 +69,6 @@ input model =
         [ 0 ]
         model.mdl
         [ Textfield.onInput (Types.GameMsg << InputChat)
+        , Textfield.value model.game.chatInput
+        , cs "chatbox--actions-input"
         ]
