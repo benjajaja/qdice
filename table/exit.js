277e44a3053654c965976989b7a9304049172dec
Benjamin Große <ste3ls@gmail.com>
2018-01-04 17:55:07 +0100

    anonymous exit

--- table/exit.js
+++ table/exit.js
@@ -1,11 +1,11 @@
 const publish = require('./publish');
 
 module.exports = async (user, table, clientId) => {
-  publish.exit(table, user.name);
+  publish.exit(table, user ? user.name : null);
   publish.event({
     type: 'exit',
     table: table.name,
-    userId: user.id,
+    userId: user ? user.id : undefined,
   });
 };
 
