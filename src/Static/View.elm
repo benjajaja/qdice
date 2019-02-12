281152be65acba47c4135d036d4e29211b0ba985
Benjamin Große <ste3ls@gmail.com>
2019-02-12 10:07:39 +0100

    sticky footer

--- src/Static/View.elm
+++ src/Static/View.elm
@@ -14,7 +14,7 @@ view model page =
 
         About ->
             Markdown.toHtml [] """
-## About *¡Qué Dice!*
+## About Qdice
 
 A dice game, written in [Elm](https://elm-lang.org). A simplified Risk, inspired by [dicewars](http://www.gamedesign.jp/games/dicewars/) and its successor.
 
@@ -23,7 +23,8 @@ Created by <ste3ls@gmail.com>.
 #### Priority-High:
   * ✗ Update table list on local and other game update
   * ✗ Profile page
-  * ✗ Login with facebook, github, reddit
+  * ✗ Login with twitter, facebook, github
+  * ✗ Set password on account, login with password
   * ✗ Connect an auth method to anonymous account
 
 #### Priority-Low:
