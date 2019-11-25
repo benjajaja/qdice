b4d0104350c62195a9bd0547ac81c555387a152e
Benjamin Große <ste3ls@gmail.com>
2019-11-25 11:27:23 +0100

    deploy calls docker-compose through ssh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,7 @@
 
 rsync -az --force --delete --progress --exclude-from=rsync_exclude.txt -e "ssh -p22 " ./ gipsy@qdice.wtf:/home/gipsy/nodice2 || exit 1
 
-# ssh -tt gipsy@qdice.wtf <<'ENDSSH'
-# cd nodice
-# yarn
-# sleep 1
-# pm2 startOrReload ecosystem.config.js --env production
-# ENDSSH
+ssh -tt gipsy@qdice.wtf <<'ENDSSH'
+cd nodice2
+docker-compose up -d --build --force-recreate --remove-orphans
+ENDSSH
