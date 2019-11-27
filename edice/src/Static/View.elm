850d4647224619648fa6ae53b73b42ca17737473
Benjamin Große <ste3ls@gmail.com>
2019-11-27 14:59:49 +0100

    circleci remove submodule config, test bundlesize

--- edice/src/Static/View.elm
+++ edice/src/Static/View.elm
@@ -20,18 +20,28 @@ A dice game, written in [Elm](https://elm-lang.org). A simplified Risk, inspired
 
 Created by <ste3ls@gmail.com>.
 
+Continuous Integration: ![CircleCI](https://circleci.com/bb/easy-rider/nodice-server.svg?style=svg&circle-token=2e6482333d4ab9f6553381e0bfb152e93a43f9fc)
+
+There is a [subreddit in place as a community forum](https://reddit.com/r/qdice), if you post there it will be seen by the site's owner.
+
 ## Roadmap
 
-#### High Priority:
-  * ✔ Flag "ready" to start
-  * ✗ AI bots
-
-#### Priority-Low:
-  * ✗ Show "who's turn it is" better
-  * ✔ Fullscreen tooltip
-  * ✗ Profile page
-  * ✗ Set password on account, login with password
+#### High priority:
   * ✗ Connect an auth method to anonymous account
-  * ✗ Replay a game
+  * ✗ Delete account (GDPR)
+  * ✗ ToS on signup/login (GDPR)
+
+#### Low priority:
+  * ✗ Login with password
+  * ✗ Profile page
   * ✗ Login with twitter, facebook, github
+  * ✗ Show "who's turn it is" better
+  * ✗ Replay a game
+
+#### Complete:
+  * ✔ Flag "ready" to start
+  * ✔ Bots
+  * ✔ Fullscreen tooltip
+  * ✔ Set password on account
+  * ...
 """
