e5873c19383b4d419dea0523ed5eaf3f258773d1
Benjamin Große <ste3ls@gmail.com>
2019-12-14 17:38:26 +0100

    less downtime on deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-rsync -az --force --delete --progress --exclude-from=rsync_exclude.txt -e "ssh -p22 " ./ gipsy@qdice.wtf:/home/gipsy/nodice2 || exit 1
+set -e
+
+rsync -az --force --delete --progress --exclude-from=rsync_exclude.txt -e "ssh -p22 " ./ gipsy@qdice.wtf:/home/gipsy/nodice || exit 1
 
 ssh -tt gipsy@qdice.wtf <<'ENDSSH'
-cd nodice2
+cd nodice
 docker-compose pull nodice
 docker-compose build --no-cache
-docker-compose down --volumes
-docker-compose up -d --force-recreate --remove-orphans
+docker-compose stop nodice nginx telegram
+docker-compose rm --force -v nodice nginx
+docker volume rm nodice_statics
+docker-compose up -d --remove-orphans
+docker-compose restart telegram
 exit 0
 ENDSSH
