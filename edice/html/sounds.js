15a65e015e313418cc7ad5514762c632e56a05f0
Benjamin Große <ste3ls@gmail.com>
2020-01-03 17:06:05 +0100

    use fetch for oggs

--- edice/html/sounds.js
+++ edice/html/sounds.js
@@ -14,39 +14,34 @@ setTimeout(function() {
   window.AudioContext = window.AudioContext || window.webkitAudioContext;
   keys.forEach(function(key) {
     var url = "sounds/" + key + ".ogg";
-    var request = new XMLHttpRequest();
-    request.open("GET", url, true);
-    request.responseType = "arraybuffer";
-    request.onload = function() {
-      var response = request.response;
-
-      sounds[key] = function() {
-        var context = new AudioContext();
-        context.decodeAudioData(
-          response,
-          function(buffer) {
-            if (!buffer) {
-              console.error("error decoding file data: " + url);
-              return;
+    fetch(url)
+      .then(function(response) {
+        return response.arrayBuffer();
+      })
+      .then(function(arrayBuffer) {
+        sounds[key] = function() {
+          var context = new AudioContext();
+          context.decodeAudioData(
+            arrayBuffer,
+            function(buffer) {
+              if (!buffer) {
+                console.error("error decoding file data: " + url);
+                return;
+              }
+              sounds[key] = function() {
+                var source = context.createBufferSource();
+                source.buffer = buffer;
+                source.connect(context.destination);
+                source.start(0);
+              };
+              sounds[key]();
+            },
+            function(error) {
+              console.error("decodeAudioData error", error);
             }
-            sounds[key] = function() {
-              var source = context.createBufferSource();
-              source.buffer = buffer;
-              source.connect(context.destination);
-              source.start(0);
-            };
-            sounds[key]();
-          },
-          function(error) {
-            console.error("decodeAudioData error", error);
-          }
-        );
-      };
-    };
-    request.onerror = function() {
-      console.error("BufferLoader: XHR error");
-    };
-    request.send();
+          );
+        };
+      });
   });
 }, 1);
 
