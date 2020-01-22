0d35f8a6d5dd75f0b141f3b6c09a36831f2194e4
Benjamin Große <ste3ls@gmail.com>
2020-01-23 00:28:33 +0100

    remove topwebgames voting stuff

--- edice/src/Game/View.elm
+++ edice/src/Game/View.elm
@@ -377,32 +377,6 @@ playerBox model =
                                     []
                                )
                     ]
-                        ++ (if not <| List.member "topwebgames" user.voted then
-                                [ div [ class "edPlayerBox__settings" ]
-                                    [ div [] [ text <| "You can get 1000" ++ pointsSymbol ++ " by voting for qdice.wtf" ]
-                                    , div []
-                                        [ a
-                                            [ href <|
-                                                "http://topwebgames.com/in.aspx?ID=11959&uid="
-                                                    ++ user.id
-                                                    ++ (case model.backend.clientId of
-                                                            Just clientId ->
-                                                                "&client_id=" ++ clientId
-
-                                                            Nothing ->
-                                                                ""
-                                                       )
-                                            , target
-                                                "_blank"
-                                            ]
-                                            [ text "Vote for qdice" ]
-                                        ]
-                                    ]
-                                ]
-
-                            else
-                                []
-                           )
 
                 Anonymous ->
                     [ div [] [ text "You're not logged in." ]
