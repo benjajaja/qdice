c33945e0f10b98630b1e268cb66002a35102a69e
Benjamin Große <benjamin@midokura.com>
2020-03-22 16:24:11 +0100

    don't rely on "nodice" dir prefix of docker-compose

--- scripts/psql.sh
+++ scripts/psql.sh
@@ -2,5 +2,5 @@
 export $(cat .env | xargs)
 export $(cat .local_env | xargs)
 
-docker run -it --rm --network qdice -e PGPASSWORD=$POSTGRES_PASSWORD nodice_postgres \
+docker run -it --rm --network qdice -e PGPASSWORD=$POSTGRES_PASSWORD $(basename $(pwd))_postgres \
   psql -U bgrosse -h postgres -d nodice
--- scripts/restart.sh
+++ scripts/restart.sh
@@ -4,5 +4,5 @@ docker-compose build --no-cache
 docker-compose stop nodice nginx telegram
 docker-compose rm --force -v nodice nginx
 docker volume rm nodice_statics
-docker-compose -p nodice up -d --remove-orphans
+docker-compose -p $(basename $(pwd)) up -d --remove-orphans
 
