155c01e7c0b51a2ac4518899705e6632135a93ac
Benjamin Große <ste3ls@gmail.com>
2020-03-29 14:07:55 +0200

    generate starting positions with docker

---
+++ scripts/starting_positions.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+docker build --tag starting ./starting_positions
+
+rm -rf starting_positions/maps
+mkdir -p starting_positions/maps/adj_mat
+mkdir -p starting_positions/maps/output
+cp map-sources.json starting_positions/maps/
+
+MAPS=""
+for f in edice/maps/*
+do
+  NAME=$(head -n 1 $f)
+  MAPS="${MAPS} ${NAME}"
+done
+
+echo $MAPS
+
+docker run -e "MAPS=${MAPS}" -v "$(realpath starting_positions/maps):/app/maps" starting
