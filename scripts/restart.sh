cf8f235568cbd9a4482358eccad8404b676bdb93
Benjamin Große <benjamin@midokura.com>
2020-03-29 15:57:53 +0200

    multi-stage build nginx for Elm statics
    Build Elm frontend with node, then server with nginx image.
    Much better than sharing a volume across containers.

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-docker-compose build --no-cache
 docker-compose stop nodice nginx telegram
 docker-compose rm --force -v nodice nginx
-docker volume rm nodice_statics
 docker-compose -p $(basename $(pwd)) up -d --remove-orphans
 
