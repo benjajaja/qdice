0da8eabeefc790d8df3e62c5863ee5442750d5c3
Benjamin Große <benjamin@midokura.com>
2020-04-09 01:06:21 +0200

    unit test Map adjacency and Land.isBordering

---
+++ edice/tests/MapsTest.elm
@@ -0,0 +1,47 @@
+module MapsTest exposing (..)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Helpers exposing (combine)
+import Land
+import Maps
+import Tables exposing (Map(..))
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    describe "Maps"
+        [ describe "Maps.load"
+            [ test "loads Planeta" <|
+                \_ ->
+                    case Maps.load Planeta of
+                        Err err ->
+                            Expect.fail err
+
+                        Ok map ->
+                            Expect.equal 42 <| List.length map.lands
+            ]
+        , describe "Land.isBordering"
+            [ test "adjacency" <|
+                \_ ->
+                    Expect.equal
+                        ([ ( "🍏", "🌙" ), ( "\u{1F920}", "👑" ) ]
+                            |> List.map
+                                (\( a, b ) ->
+                                    case Maps.load Planeta of
+                                        Err _ ->
+                                            Nothing
+
+                                        Ok map ->
+                                            Maybe.map2
+                                                (Land.isBordering map)
+                                                (Land.findLand a map.lands)
+                                                (Land.findLand b map.lands)
+                                )
+                            |> combine
+                            |> Maybe.map (List.all identity)
+                        )
+                        (Just True)
+            ]
+        ]
