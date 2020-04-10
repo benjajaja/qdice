6155d114c8d8e872921df15c0b1b6157a20d9d54
Benjamin Große <ste3ls@gmail.com>
2020-04-10 13:59:57 +0200

    expand the About page

--- edice/src/Static/View.elm
+++ edice/src/Static/View.elm
@@ -20,7 +20,40 @@ A dice game, written in [Elm](https://elm-lang.org). A simplified Risk, inspired
 
 Created by <ste3ls@gmail.com>.
 
+## Development
+
 Check the sticky **[Development post](https://www.reddit.com/r/Qdice/comments/epy4hh/development_updates/)** on the subreddit that will be updated. You can post issues and wishes on that post or generally in the sub.
 
 There is also the **[changelog](/changelog)** with a complete list of all changes that go live.
+
+Qdice is Free Open Source Software! See repo here: [github.com/gipsy-king/qdice](https://github.com/gipsy-king/qdice).
+
+Before making a PR, I suggest to get in contact with me however you want, an issue in github is a good start.
+
+## Tech rundown
+
+* **Elm** - the main motivation was to learn [Elm](https://elm-lang.org). It's an awesome project
+  that is quite different from conventional "enterprise" web development. Simply fun!
+* **Node.js** - This has been qute a ride since the wirst prototype.
+  1. First there was the most simple imperative mutable OOP style, with all games in memory.
+     The problem was that when it eventually crashed, any games were aborted.
+  2. Next iteration had one process per game, with the game in-memory, with a more
+     functional immutable type code-style. The idea was that if one game crashes, it shouldn't
+     affect other running games. This turned out to be terrible: total memory usage was huge
+     and a game was still lost if a server crashed anyway.
+  3. Current iteration is the standard node.js way, with all state in DB. Actually there is an
+     in-memory cache layer, but a game will resume fine on a crash or restart.
+  There is some sort of command-queue for processing all game events, both from players
+  and from internal ticks.
+* **MQTT** - with emqx. Web clients talk to the server over MQTT. The advantages are that there is a broker between
+  client and server, so if the (node.js) server eventually crashes (and resurrects), clients don't even notice. Some
+  "topic" concept is also provided. It's probably a bit overkill, because clients don't talk to each
+  other.
+* **PostgreSQL** - Solid relational database with good support for JSON columns when needed.
+* **Docker** - everything is built and run with docker.
+* **Testing** - Elm needs almost no unit test due to it's guarantees, there is much more benefit in
+  having end-to-end tests where unit tests would be too cumbersome and overblown.
+  End-to-end tests are run with docker and play a real game from beginning to end.
+  The node.js server has a greater number of unit tests.
+* **CI/CD** - Everything is built and e2e-tested before deploying automatically.
 """
