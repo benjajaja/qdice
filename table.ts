b9edb6d4a0da4a9918739f71c9d1a56d93c1a16a
Benjamin Große <ste3ls@gmail.com>
2020-04-15 12:13:37 +0200

    show other player's toggle-ready

--- table.ts
+++ table.ts
@@ -351,7 +351,11 @@ export const processCommand = async (table: Table, command: Command) => {
     if (eliminations) {
       await processEliminations(newTable, eliminations);
     }
-    if (["Join", "Leave", "Start", "EndGame"].indexOf(command.type) !== -1) {
+    if (
+      ["Join", "Leave", "Start", "EndGame", "ToggleReady"].indexOf(
+        command.type
+      ) !== -1
+    ) {
       logger.debug(`tableStatus: ${command.type}`);
       logger.debug(`players: ${newTable.players.length}`);
       publish.tableStatus(newTable);
