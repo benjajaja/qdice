84d324b30b2c4b672821996019925d783a5f98b1
Benjamin Große <ste3ls@gmail.com>
2021-03-23 15:49:07 +0100

    use stock postgres

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -3,7 +3,7 @@ set -e
 
 docker-compose build
 
-./scripts/toast.sh "Server is restarting for an update..." || true
+# ./scripts/toast.sh "Server is restarting for an update..." || true
 docker-compose stop nodice nginx beancounter
 docker-compose rm --force -v nodice nginx beancounter
 docker-compose -p $(basename $(pwd)) up -d --remove-orphans
