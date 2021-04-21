8754302b6a96dce386441b7b194a1273cc048f3d
Benjamin Große <ste3ls@gmail.com>
2021-04-21 09:52:57 +0200

    fix backup scripts

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -10,14 +10,15 @@ export $(cat .local_env | xargs)
 
 DATE="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
 
+echo "Streaming pg_dump to S3 (${DATE})..."
 docker run -i --rm --network qdice -e PGPASSWORD=$POSTGRES_PASSWORD postgres:9.6 \
   pg_dump -U bgrosse -h postgres -d nodice \
   -Z 9 | aws s3 cp - s3://qdice-postgres/backup_${DATE}.dump.gz
 echo "Streamed DB archive to S3: backup_${DATE}.dump.gz"
 
-DIR="/mnt/backups/backup_${DATE}"
-FILENAME="files_${DATE}.tgz"
-FILE_PATH="${DIR}/${FILENAME}"
+DIR="/tmp/backup_${DATE}"
+FILENAME="backup_${DATE}.archive.tgz"
+FILE_PATH="/tmp/${FILENAME}"
 
 
 mkdir -p $DIR
--- scripts/psql.sh
+++ scripts/psql.sh
@@ -2,5 +2,5 @@
 export $(cat .env | xargs)
 export $(cat .local_env | xargs)
 
-docker run -it --rm --network qdice -e PGPASSWORD=$POSTGRES_PASSWORD $(basename $(pwd))_postgres \
+docker run -it --rm --network qdice -e PGPASSWORD=$POSTGRES_PASSWORD postgres:9.6 \
   psql -U bgrosse -h postgres -d nodice
